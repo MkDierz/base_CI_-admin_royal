2bed0a97dd2d42b3402f0d7665198ec58fc4298d
mkdierz <msyibbran24@gmail.com>
2019-12-13 18:37:26 +0700

    tweak header

--- application/views/home/_partials/header.php
+++ application/views/home/_partials/header.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>RoyalUI Admin</title>
+    <title><?php echo $title; ?></title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendors/ti-icons/css/themify-icons.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendors/base/vendor.bundle.base.css">
@@ -15,7 +15,7 @@
     <!-- inject:css -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
     <!-- endinject -->
-    <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/favicon.png" />
+    <link rel="shortcut icon" href="<?php echo base_url('assets/images/').$icon; ?>" />
 </head>
 <body>
     <div class="container-scroller">
