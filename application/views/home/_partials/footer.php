4e8fdc02deee641934666bb9188c812df6c726ad
mkdierz <msyibbran24@gmail.com>
2019-12-13 17:50:36 +0700

    ok

--- application/views/home/_partials/footer.php
+++ application/views/home/_partials/footer.php
@@ -15,19 +15,19 @@
     <!-- container-scroller -->
 
     <!-- plugins:js -->
-    <script src="vendors/base/vendor.bundle.base.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendors/base/vendor.bundle.base.js"></script>
     <!-- endinject -->
     <!-- Plugin js for this page-->
-    <script src="vendors/chart.js/Chart.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendors/chart.js/Chart.min.js"></script>
     <!-- End plugin js for this page-->
     <!-- inject:js -->
-    <script src="js/off-canvas.js"></script>
-    <script src="js/hoverable-collapse.js"></script>
-    <script src="js/template.js"></script>
-    <script src="js/todolist.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/off-canvas.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/hoverable-collapse.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/template.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/todolist.js"></script>
     <!-- endinject -->
     <!-- Custom js for this page-->
-    <script src="js/dashboard.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/dashboard.js"></script>
     <!-- End custom js for this page-->
 </body>
 
--- application/views/home/_partials/header.php
+++ application/views/home/_partials/header.php
@@ -7,15 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>RoyalUI Admin</title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
-    <link rel="stylesheet" href="vendors/base/vendor.bundle.base.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendors/ti-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendors/base/vendor.bundle.base.css">
     <!-- endinject -->
     <!-- plugin css for this page -->
     <!-- End plugin css for this page -->
     <!-- inject:css -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
     <!-- endinject -->
-    <link rel="shortcut icon" href="images/favicon.png" />
+    <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/favicon.png" />
 </head>
 <body>
     <div class="container-scroller">
