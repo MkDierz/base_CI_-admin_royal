997521913c22be3e17c963c7e28579c27bf0fb18
mkdierz <msyibbran24@gmail.com>
2019-12-13 17:23:45 +0700

    update config

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'localhost/base_ci_';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
