c0a9178a64f052a99fff149cc4e41c928dd96365
mkdierz <msyibbran24@gmail.com>
2019-12-13 18:37:43 +0700

    tweak footer

--- application/views/home/_partials/footer.php
+++ application/views/home/_partials/footer.php
@@ -2,8 +2,8 @@
                 <!-- partial:partials/_footer.html -->
                 <footer class="footer">
                     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2018 <a href="https://www.templatewatch.com/" target="_blank">Templatewatch</a>. All rights reserved.</span>
-                        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ml-1"></i></span>
+                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2019 S_Mulaesyi. All rights reserved.</span>
+                        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Teknik Informatika Unimal </span>
                     </div>
                 </footer>
             <!-- partial -->
@@ -15,19 +15,19 @@
     <!-- container-scroller -->
 
     <!-- plugins:js -->
-    <script src="<?php echo base_url(); ?>assets/vendors/base/vendor.bundle.base.js"></script>
+    <script src="<?php echo base_url('assets/'); ?>vendors/base/vendor.bundle.base.js"></script>
     <!-- endinject -->
     <!-- Plugin js for this page-->
-    <script src="<?php echo base_url(); ?>assets/vendors/chart.js/Chart.min.js"></script>
+    <script src="<?php echo base_url('assets/'); ?>vendors/chart.js/Chart.min.js"></script>
     <!-- End plugin js for this page-->
     <!-- inject:js -->
-    <script src="<?php echo base_url(); ?>assets/js/off-canvas.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/hoverable-collapse.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/template.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/todolist.js"></script>
+    <script src="<?php echo base_url('assets/'); ?>js/off-canvas.js"></script>
+    <script src="<?php echo base_url('assets/'); ?>js/hoverable-collapse.js"></script>
+    <script src="<?php echo base_url('assets/'); ?>js/template.js"></script>
+    <script src="<?php echo base_url('assets/'); ?>js/todolist.js"></script>
     <!-- endinject -->
     <!-- Custom js for this page-->
-    <script src="<?php echo base_url(); ?>assets/js/dashboard.js"></script>
+    <script src="<?php echo base_url('assets/'); ?>js/dashboard.js"></script>
     <!-- End custom js for this page-->
 </body>
 
