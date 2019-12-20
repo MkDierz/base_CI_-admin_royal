1d3e5aeee0f1f852bf5d35aace1ecfdb9cbfb29d
mkdierz <msyibbran24@gmail.com>
2019-12-20 18:54:43 +0700

    tweak navbar

--- application/views/home/_partials/navbar.php
+++ application/views/home/_partials/navbar.php
@@ -1,29 +1,17 @@
 <!-- partial:partials/_navbar.html -->
-<nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-    <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo mr-5" href="<?php echo base_url(); ?>"><img src="<?php echo base_url('assets/'); ?>images/logo.png" class="mr-2" alt="logo"/></a>
-        <a class="navbar-brand brand-logo-mini" href="<?php echo base_url(); ?>"><img src="<?php echo base_url('assets/'); ?>images/logo-mini.png" alt="logo"/></a>
+<nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row shadow-none">
+    <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center bg-success " style="z-index:3;">
+        <a class="navbar-brand brand-logo" href="<?php echo base_url(); ?>"><img src="<?php echo base_url('assets/'); ?>images/logo.png"  alt="logo" style="height:auto; width:auto; max-height:60px;"/></a>
+        <a class="navbar-brand brand-logo-mini" href="<?php echo base_url(); ?>"><img src="<?php echo base_url('assets/'); ?>images/logo-mini.png" alt="logo" style="height:55px; width:auto;"/></a>
     </div>
-    <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+    <div class="navbar navbar-menu-wrapper d-flex align-items-center justify-content-end bg-success" style="z-index:1;">
         <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-            <span class="ti-view-list"></span>
+            <span class="ti-view-list text-light"></span>
         </button>
-        <!-- <ul class="navbar-nav mr-lg-2">
-            <li class="nav-item nav-search d-none d-lg-block">
-                <div class="input-group">
-                    <div class="input-group-prepend hover-cursor" id="navbar-search-icon">
-                        <span class="input-group-text" id="search">
-                            <i class="ti-search"></i>
-                        </span>
-                    </div>
-                    <input type="text" class="form-control" id="navbar-search-input" placeholder="Search now" aria-label="search" aria-describedby="search">
-                </div>
-            </li>
-        </ul> -->
         <ul class="navbar-nav navbar-nav-right">
-            <li class="nav-item dropdown mr-1">
+            <!-- <li class="nav-item dropdown mr-1">
                 <a class="nav-link count-indicator dropdown-toggle d-flex justify-content-center align-items-center" id="messageDropdown" href="#" data-toggle="dropdown">
-                    <i class="ti-email mx-0"></i>
+                    <i class="ti-email mx-0 text-light"></i>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="messageDropdown">
                     <p class="mb-0 font-weight-normal float-left dropdown-header">Messages</p>
@@ -37,8 +25,8 @@
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
-                    <i class="ti-bell mx-0"></i>
-                    <!-- <span class="count"></span> -->
+                    <i class="ti-bell mx-0 text-light" ></i>
+                    <span class="count"></span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="notificationDropdown">
                     <p class="mb-0 font-weight-normal float-left dropdown-header">Notifications</p>
@@ -56,7 +44,7 @@
                         </div>
                     </a>
                 </div>
-            </li>
+            </li> -->
             <li class="nav-item nav-profile dropdown">
                 <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
                     <img src="<?php echo base_url('assets/'); ?>images/faces/face28.jpg" alt="profile"/>
